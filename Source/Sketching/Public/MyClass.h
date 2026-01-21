a628e5947d1f42b05350e860f8d26deca8ede717
Rippchord <silvermonkey14@gmail.com>
2026-01-21 08:47:38 -0500

    Connecting Sketch UE5 project

---
+++ Source/Sketching/Public/MyClass.h
@@ -0,0 +1,15 @@
+// SFill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class SKETCHING_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
